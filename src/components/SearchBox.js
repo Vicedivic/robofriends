b4a846e8a70fa6429238d89c63581e2447155edb
Victor <victor@lfpost.dk>
2020-03-03 17:43:44 +0100

    Add accessibility hack for search input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,13 @@ import React from "react";
 const SearchBox = ({ searchChange }) => {
   return (
     <div>
+      <label htmlFor="search" style={{ fontSize: 0, position: "absolute" }}>Search</label>
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
         onChange={searchChange}
-        aria-label="Search"
+        id="search"
       />
     </div>
   );
