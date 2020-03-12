131736835d11414dad2dd4bdfd5cf088195ad3bf
Victor <victor@lfpost.dk>
2020-03-12 14:47:22 +0100

    Update title on app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 
 const Header = () => {
-  return ( <h1 className="f1">
-  RoboFriends</h1>);
+  return <h1 className="f1">RoboFriends - Where Robots Meet</h1>;
 };
 
-export default memo( Header)
+export default memo(Header);
